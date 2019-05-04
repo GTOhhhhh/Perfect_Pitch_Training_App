2701f354e9e6442d70e7c999b72bea6c7cf2a033
Karl Spiegel <karlspiegel@Administrators-MacBook-Pro.local>
2019-05-05 07:43:47 +1000

    updated timings and modes

--- AP.py
+++ AP.py
@@ -18,11 +18,10 @@ init()
 
 
 # 32 notes for each tone, target note added 4, 5 or  times
-def gen_seq(target, k=28, add=(4, 5, 6)):
+def gen_seq(target, k=90, add=14):
     seq = list(all)
     seq.remove(target)
     seq = random.choices(seq, k=k)
-    add = random.choice(add)
     for i in range(add):
         seq.append(target)
     random.shuffle(seq)
@@ -37,15 +36,15 @@ def play(path):
 def run_seq(test_seq, hard=False, speed=1.5, display=False):
     for target in test_seq:
         print('target: ', target)
-        target_seq = gen_seq(target) if not hard else gen_seq(target, 72)
+        target_seq = gen_seq(target) if not hard else gen_seq(target, 90)
 
-        if target in naturals:
-            play('./notes/sung/{}s.wav'.format(target))
-            time.sleep(0.1)
-        else:
-            play('./notes/sung/{}.wav'.format(target))
+        # if target in naturals:
+        #     play('./notes/sung/{}s.wav'.format(target))
+        #     time.sleep(0.1)
+        # else:
+        #     play('./notes/sung/{}.wav'.format(target))
 
-        time.sleep(1)
+        time.sleep(2.5)
 
         for note in target_seq:
             path = "./notes/" + note + '/' + random.choice(os.listdir("./notes/" + note + "/"))
@@ -57,7 +56,7 @@ def run_seq(test_seq, hard=False, speed=1.5, display=False):
                 print(colored('that was the target note', 'green'))
             else:
                 if display:
-                    out = note
+                    out = note  
                     if out == 'C#':
                         out = 'Db'
                     elif out == 'D#':
@@ -81,13 +80,14 @@ def run(test_seq):
         if sys.argv[1] == 'F':
             speed = 0.7
         elif sys.argv[1] == 'S':
-            speed = 1.7
+            speed = 1.8
     if args >= 3:
         if sys.argv[2] == 'W':
             test_seq = weak
             print(f"{test_seq}")
         elif sys.argv[2] == 'D':
             display = True
+
     if args >= 4:
         if sys.argv[3] == 'D':
             display = True
@@ -101,5 +101,4 @@ def run(test_seq):
     #     return
 
 
-run(all)
 run(all)
